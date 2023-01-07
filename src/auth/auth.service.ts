dc6a77cf50b54a4fba359fbccccf6c58d2d7144e
Omar Lopez <contacto.omarlopez@gmail.com>
2023-01-07 12:11:10 -0400

    users

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,43 @@
+import { Injectable, BadRequestException, InternalServerErrorException } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './entities/user.entity';
+
+
+@Injectable()
+export class AuthService {
+
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>
+  ){}
+
+
+
+  async create(createUserDto: CreateUserDto) {
+
+
+    try{
+      const user = this.userRepository.create(createUserDto);
+      await this.userRepository.save(user);
+      return user;
+    }
+    catch(error){
+      this.handleDBErrors(error);
+    }
+
+  }
+
+  private handleDBErrors(error:any):never{
+    if(error.code === '23505'){
+      throw new BadRequestException(error.detail)
+    }
+    console.log(error)
+    throw new InternalServerErrorException('Please check server logs');
+  }
+
+
+
+
+}
